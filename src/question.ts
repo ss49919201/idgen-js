6f0cae010665e9ee904d666e602eccb377afa6aa
ss49919201 <sakashin10291029@gmail.com>
2024-11-03 11:50:21 +0900

    feat: add question manager input and output

--- src/question.ts
+++ src/question.ts
@@ -1,11 +1,15 @@
 import { createInterface } from "node:readline/promises";
+import { ReadLineOptions } from "readline";
 
 export interface QuestionManager {
   ask(question: string): Promise<string>;
   close(): void;
 }
 
-export interface QuestionManagerOptions {}
+export interface QuestionManagerOptions {
+  input?: ReadLineOptions["input"];
+  output: ReadLineOptions["output"];
+}
 
 export const newQuestionManager: (
   opt?: QuestionManagerOptions
